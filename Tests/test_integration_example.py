5bb81188c808c6e9e8b9d35dfebebe6d47bcffe7
arthur-leg <129770298+arthur-leg@users.noreply.github.com>
2026-04-29 16:49:36 +0200

    test1

--- Tests/test_integration_example.py
+++ Tests/test_integration_example.py
@@ -1,8 +1,10 @@
-import sys
 import os
+import sys
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app import app as flask_app
+
 
 def test_index_endpoint():
     client = flask_app.test_client()
